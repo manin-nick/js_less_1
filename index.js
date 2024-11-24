a6b603afced4940d461ef80b387475f2ef331138
manin-nick <manin.nick@gmail.com>
2024-11-24 20:11:38 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// let isAdult = true
+// let isStudent = false
+
+// console.log(isAdult)
+// console.log(isStudent)
+
+// let unknown
+// console.log(unknown)
+
+// let empty = null
+// console.log(empty)
+
+// let sym1 = Symbol('Тестовый текст')
+// let sym2 = Symbol('Тестовый текст')
+// console.log(sym1 === sym2)
+
+// let str1 = "text"
+// let str2 = "text" 
+// console.log(str1 === str2)
+
+// let bigNum = BigInt(999999999999999999999999)
+
+// let person = {
+//     name: "Nick",
+//     age: 36
+// }
+// console.log(person)
+// console.log(person.age)
+
+// let numbers = [1, 2, 3, 4, 5, 'hi']
+// console.log(numbers[0])
+
+
+// let str10 = 'Nickolay'
+// let number10 = 20
+// console.log(str10 + number10)
+
+// let number11 = 20
+// let str11 = String(number11)
+// console.log(number11)
+// console.log(str11)
+
+// let str12 = '12345'
+// let num12 = Number(str12)
+// console.log(str12)
+// console.log(num12)
+
+// let notEmpty = 'text'
+// let isEmpty
+// let boolValue1 = Boolean(notEmpty)
+// let boolValue2 = Boolean(isEmpty)
+// console.log(notEmpty)
+// console.log(isEmpty)
+// console.log(boolValue1)
+// console.log(boolValue2)
+
+
+let a = '5'
+let b = 2
+console.log(a + b)
+console.log(a * b)
